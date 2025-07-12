6b0f05b3248a3cbf93e54fbd3590d2a654f0df39
vommidapuchinni <161326469+vommidapuchinni@users.noreply.github.com>
2025-07-12 14:21:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,18 @@ module "vpc" {
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
   enable_nat_gateway = true
   single_nat_gateway = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Environment = "production"
+    Project     = "medusa"
+  }
 }
 
 resource "aws_security_group" "alb" {
