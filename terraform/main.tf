e8459910d7355af39230e214ddd3b3f2038eda66
root <root@ip-172-31-91-2.ec2.internal>
2025-07-12 08:22:05 +0000

    2nd commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,35 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
 }
+
+resource "aws_security_group" "alb" {
+  name        = "alb-sg"
+  description = "Allow HTTP/HTTPS"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
